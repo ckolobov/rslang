3f858efd6ed1399c3718944edc4d9222931961ad
MilanaKard <n8m91213@gmail.com>
2022-02-10 15:17:50 +0300

    feat: change authorization form

--- rslang/src/components/common/AuthorizationForm.ts
+++ rslang/src/components/common/AuthorizationForm.ts
@@ -7,10 +7,19 @@ import '../../scss/components/_authorization-form.scss';
 class AuthorizationForm implements Component {
   private class: string;
   private id: string;
+  private type: 'Sign in' | 'Sign up';
+  private action;
 
   public constructor(options) {
     this.class = options.class;
     this.id = options.id;
+    this.type = 'Sign in';
+    this.action = this.loginUser;
+  }
+
+  private text = {
+    'Sign in': ['Sign in', 'Don\'t have an account?', 'Log in'],
+    'Sign up': ['Sign up', 'Already have an account?', 'Create an account']
   }
 
   static authorizationInfo = {
@@ -22,14 +31,15 @@ class AuthorizationForm implements Component {
     const authorizationButton = await Drawer.drawComponent(Button, {
       id: 'authorization-form-button',
       class: 'login__button',
-      text: 'Log in',
+      text: `${this.text[this.type][2]}`,
     });
+    
     const view = `
     <div id="${this.id ? this.id : ''}" class="${this.class ? this.class : ''}">
       <div class="login__body">
         <form class="login__form" novalidate>
           <div class="login__close"></div>
-          <h1>Sign in</h1>
+          <h1>${this.type}</h1>
           <div class="login__content">
             <input
               id="email"
@@ -43,8 +53,8 @@ class AuthorizationForm implements Component {
               placeholder="password"
               type="password"/>
             <p class="error login__error-text" id="password-error"></p>
-              <p class="login__text">
-                If you don't have an account, a new one will be created.
+              <p class="login__text link">
+                ${this.text[this.type][1]}
               </p>
             ${authorizationButton}
           </div>
@@ -69,41 +79,59 @@ class AuthorizationForm implements Component {
   private showErrorMessage(id: 'password-error' | 'email-error', errorText: string) {
     const errorElement = document.getElementById(id) as HTMLElement;
     errorElement.innerHTML = errorText;
+    const input = errorElement.previousElementSibling;
+    if(input?.tagName === 'INPUT') {
+      input.classList.add('error');
+    }
   }
+
+  private changeForm() {
+    if (this.type === 'Sign in') {
+      this.type = 'Sign up';
+      this.action = this.createUser;
+    } else {
+      this.type = 'Sign in';
+      this.action = this.loginUser; 
+    }
+  }
+
+  private async createUser(email: string, password: string): Promise<void> {
+    const res = await Request.createUser({
+      name: 'user',
+      email,
+      password,
+    });
+    if (res.error) {
+      res.error.errors.forEach((err) => { 
+        const id = err.message[1] === 'e' ? 'email-error' : 'password-error';
+        this.showErrorMessage(id, err.message);
+      })
+      return;
+  } 
+  await this.loginUser(email, password);
+}
 
   private async loginUser(email: string, password: string): Promise<void> {
     try {
       const res = await Request.loginUser({ email, password });
       AuthorizationForm.authorizationInfo.isAuthorized = true;
       Object.assign(AuthorizationForm.authorizationInfo, res);
-      console.log(AuthorizationForm.authorizationInfo);
       this.setLocalStorage();
-      console.log(res);
     } catch (error) {
       if (error == 'SyntaxError: Unexpected token F in JSON at position 0') {
         this.showErrorMessage('password-error', 'Wrong password');
-        return;
+      } 
+      if (error == 'SyntaxError: Unexpected token C in JSON at position 0') {
+        this.showErrorMessage('email-error', `Account with this email doesn't exist.`);
       }
-      const res = await Request.createUser({
-        name: 'user',
-        email,
-        password,
-      });
-      console.log(res);
-      if (res.error) {
-        res.error.errors.forEach((err) => { 
-          const id = err.message[1] === 'e' ? 'email-error' : 'password-error';
-          this.showErrorMessage(id, err.message);
-        })
-        return;
-      }
-      await this.loginUser(email, password);
     }
   }
 
   public async after_render(): Promise<void> {
     const formPopup = document.getElementById(this.id) as HTMLElement;
     const form = formPopup.querySelector('form') as HTMLFormElement;
+    const switchForm = form.querySelector('.login__text') as HTMLElement;
+    const title = form.querySelector('H1') as HTMLElement;
     const inputs = formPopup.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
     const button = document.getElementById('authorization-form-button') as HTMLButtonElement;
 
@@ -122,7 +150,7 @@ class AuthorizationForm implements Component {
       event.preventDefault();
       const email: string = form.elements['email'].value;
       const password: string = form.elements['password'].value;
-      await this.loginUser(email, password);
+      await this.action(email, password);
       if (AuthorizationForm.authorizationInfo.isAuthorized) {
         const button = document.getElementById('authorization-button') as HTMLElement;
         button.innerHTML = 'Log out';
@@ -130,8 +158,18 @@ class AuthorizationForm implements Component {
       }
     });
 
-    inputs.forEach((input) => {
+    switchForm.addEventListener('click', () => {
+      this.changeForm();
+      inputs[1].focus();
+      inputs[0].focus();
+      title.innerHTML = this.type;
+      switchForm.innerHTML = this.text[this.type][1];
+      button.innerHTML = this.text[this.type][2];
+    });
+
+    inputs.forEach((input) => {   
       input.addEventListener('focus', () => {
+        input.classList.remove('error');
         if (input.nextElementSibling) input.nextElementSibling.innerHTML = '';
       });
     });
